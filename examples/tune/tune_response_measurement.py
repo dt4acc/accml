8f614ab02d60dd65f9ddb76ab6f7bdd6b98514ca
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2025-08-20 12:10:03 +0200

    [TASK] store data

--- examples/tune/tune_response_measurement.py
+++ examples/tune/tune_response_measurement.py
@@ -5,6 +5,7 @@ from ophyd import Signal
 
 from accml.app.tune.tune_measurement import tune
 from accml.core.bl.liasion_translator_setup import load_managers
+from accml.core.model.identifiers import LatticeElementPropertyID
 from accml.custom.epics.bluesky_measurement_execution_engine import BlueskyMeasurementExecutionEngine
 
 # Todo: execution engine use and setup should be provided by standard measurement execution engine
@@ -24,16 +25,26 @@ if __name__ == "__main__":
     RE = RunEngine()
     [RE.subscribe(consumer) for consumer in [lt]]
     # TODO: if you need to save the results, you need mongo and then uncomment the below two lines.
-    # db = catalog["heavy_local"]
-    # RE.subscribe(db.v1.insert)
+    db = catalog["heavy_local"]
+    RE.subscribe(db.v1.insert)
     # TODO: need to address live plots
     #   here a databroker should be added so that data can be accessed
     #   later on
 
-    yp, __, _ = load_managers()
+    yp, lm, _ = load_managers()
+    # Todo: remove this line after only Q3/Q4 are flagged as tune correction magnets
+    tune_correction_quads = [name for name in yp.tune_correction_quadrupole_names() if name[1] in ["3", "4"]]
+    #  Find out to which power converters these are connected to
+    pc_names = {
+        lm.forward(LatticeElementPropertyID(name, "main_strength")).device_name:
+            name for name in tune_correction_quads
+    }
+    # Now I add a hack: I only use quadrupoles whoes power converter is unique
+    # I should rather work in device space right away
+    pc_names=list(set(pc_names))
     tune(
-       quadrupole_names=[name for name in yp.tune_correction_quadrupole_names()],
-       measurement_values=[0, 1e-5, 0, -1e-5, 0],
+       quadrupole_pc_names=pc_names,
+       measurement_values=[0, 1e0, 0, -1e0, 0],
        mexec=BlueskyMeasurementExecutionEngine(run_engine=RE),
        info_signals=info_sigs
     )
