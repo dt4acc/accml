67c1642bc0fabe5471571163ebc1fa96b2010e2d
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2025-08-20 11:13:03 +0200

    [TASK] analyse tune response per power converter, measure it per power converter

---
+++ src/accml/app/tune/model.py
@@ -0,0 +1,52 @@
+from dataclasses import dataclass
+from functools import cached_property
+from typing import Dict, Sequence
+
+
+@dataclass
+class MeasuredTuneResponseItem:
+    value: float
+    x: float
+    y: float
+
+
+@dataclass
+class MeasuredTuneResponsePerPowerConverter:
+    pc_name: str
+    col: Sequence[MeasuredTuneResponseItem]
+
+
+@dataclass
+class MeasuredTuneResponse:
+    col: Sequence[MeasuredTuneResponsePerPowerConverter]
+
+    def get(self, name) -> MeasuredTuneResponsePerPowerConverter:
+        return self._dict[name]
+
+    @cached_property
+    def _dict(self) -> Dict[str, MeasuredTuneResponsePerPowerConverter]:
+        return {item.pc_name: item for item in self.col}
+
+
+@dataclass
+class RandomVariableMomenta:
+    mean: float
+    std: float
+
+@dataclass
+class TuneResponse:
+    pc_name: str
+    x: RandomVariableMomenta
+    y: RandomVariableMomenta
+
+
+@dataclass
+class TuneResponseCollection:
+    col: Sequence[TuneResponse]
+
+    def get(self, name: str) -> TuneResponse:
+        return self._dict[name]
+
+    @cached_property
+    def _dict(self) -> Dict[str, TuneResponse]:
+        return {item.pc_name: item for item in self.col}
---
+++ src/accml/app/tune/tune_response_analysis.py
@@ -0,0 +1,58 @@
+from collections import defaultdict
+from itertools import zip_longest
+from typing import Dict, Sequence
+
+import numpy as np
+from numpy.polynomial import Polynomial
+
+from .model import MeasuredTuneResponseItem, MeasuredTuneResponsePerPowerConverter, MeasuredTuneResponse, TuneResponse, \
+    RandomVariableMomenta, TuneResponseCollection
+
+
+def data_to_model(data: Dict[str, Sequence[float]]) -> MeasuredTuneResponse:
+    # combine data per magnet
+    tmp = defaultdict(list)
+    for dev_name, val, tune_x, tune_y in zip_longest(
+            data["device_name"],
+            data["channel_value"],
+            data["tune-x-sig"],
+            data["tune-y-sig"]
+    ):
+        tmp[str(dev_name)].append(
+            MeasuredTuneResponseItem(
+                value=float(val), x=float(tune_x), y=float(tune_y)
+            )
+        )
+
+    # Now put it in a model
+    r = MeasuredTuneResponse(col=[
+            MeasuredTuneResponsePerPowerConverter(pc_name=dev_name, col=items) for dev_name, items in tmp.items()
+    ])
+    return r
+
+
+def fit_line(indep: Sequence[float], dep: Sequence[float]) -> RandomVariableMomenta:
+    """
+    Todo:
+        need to compute the standard deviation
+        find out which algorithm to be used
+
+        orbit response profits from leastsquare `lstsg` so perhaps also to use it here
+    """
+    coeffs, info =  Polynomial.fit(indep, dep, deg=1, full=True)
+    residuals, rank, singular_values, rcond = info
+    return RandomVariableMomenta(mean=float(coeffs.coef[1]), std=None)
+
+
+def fit_one_power_converter(data: MeasuredTuneResponsePerPowerConverter):
+    vals = [item.value for item in data.col]
+    xrm = fit_line(vals, [item.x for item in data.col])
+    yrm = fit_line(vals, [item.y for item in data.col])
+    return TuneResponse(pc_name=data.pc_name, x=xrm, y=yrm)
+
+
+def tune_response_analysis(data: Dict[str, Sequence[float]]):
+    prep_data = data_to_model(data)
+    return TuneResponseCollection(
+        col=[fit_one_power_converter(data) for data in prep_data.col]
+    )
