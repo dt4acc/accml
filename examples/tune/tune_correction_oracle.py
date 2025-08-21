809ac58d91da98b71658f034975dff1687363102
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2025-08-21 10:45:24 +0200

    [TASK] fixed serialisation / deserialisation

--- examples/tune/tune_correction_oracle.py
+++ examples/tune/tune_correction_oracle.py
@@ -1,17 +1,22 @@
 import jsons
 import yaml
 
-
 from accml.app.tune.model import (
+    TuneCorrectionCurrentsCollection,
     TuneResponseCollection,
     TuneResponse,
     TuneResponseForFit,
-    TuneCorrectionCurrentsCollection,
+    TuneResponseForFitItem,
 )
-from accml.app.tune.tune_correction_oracle import TuneCorrectionOracle, enrich_with_polarity
+from accml.app.tune.tune_correction_oracle import (
+    TuneCorrectionOracle,
+    enrich_with_polarity,
+)
 
 
 def sort_quadrupole_to_plane(data: TuneResponse) -> (str, TuneResponse):
+    """guess to which family or plane the quadrupoles belong to
+    """
     ratio = abs(data.x.mean / data.y.mean)
     # x is not that pronounced
     #    revisit when proper start data is there
@@ -46,8 +51,17 @@ def main():
         d = yaml.safe_load(fp)
     dm = jsons.load(d, TuneResponseCollection)
     del d
-    data_for_fit = post_process(dm)
     # represent data so that it is easier to read
+    data_for_fit = post_process(dm)
+    d = jsons.dump(data_for_fit, TuneResponseForFit)
+    d
+    with open("tune_oracle_data.yml", "wt") as fp:
+        d = yaml.dump(d, fp)
+    del d, data_for_fit
+    with open("tune_oracle_data.yml") as fp:
+        d = yaml.safe_load(fp)
+    data_for_fit = jsons.load(d, TuneResponseForFit)
+
     fit = TuneCorrectionOracle(data_for_fit)
     r = TuneCorrectionCurrentsCollection(col=fit.predict(0.1, -0.1))
     r
