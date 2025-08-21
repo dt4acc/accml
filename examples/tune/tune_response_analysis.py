24a4c460efef1ce6bcf77b11ee7529426315564c
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2025-08-21 10:45:24 +0200

    [TASK] fixed serialisation / deserialisation

--- examples/tune/tune_response_analysis.py
+++ examples/tune/tune_response_analysis.py
@@ -6,9 +6,9 @@ import yaml
 
 def main():
     db = catalog["heavy_local"]
-    uid = '1955adfd-2f94-4459-8888-4f63cbc839de'
-    uid = '0969b444-7f50-439e-9a64-2751e1678fd0'
-    run  = db[uid]
+    uid = "1955adfd-2f94-4459-8888-4f63cbc839de"
+    uid = "0969b444-7f50-439e-9a64-2751e1678fd0"
+    run = db[uid]
     data = run.primary.read()
 
     # just strip off all the extra information an xarray would provide
@@ -16,16 +16,13 @@ def main():
         "device_name": data["device_name"].data,
         "channel_value": data["channel_value"].data,
         "tune-x-sig": data["tune-x-sig"].data,
-        "tune-y-sig": data["tune-y-sig"].data
+        "tune-y-sig": data["tune-y-sig"].data,
     }
 
     result = tune_response_analysis(d)
     tmp = jsons.dump(result)
-    # Todo: currently a hack ... need to foresee appropriate methods at the data class
-    del tmp["_dict"]
     with open("tune_result.yml", "wt") as fp:
         yaml.dump(tmp, fp)
-
 
 
 if __name__ == "__main__":
--- src/accml/app/tune/tune_response_analysis.py
+++ src/accml/app/tune/tune_response_analysis.py
@@ -5,8 +5,14 @@ from typing import Dict, Sequence
 import numpy as np
 from numpy.polynomial import Polynomial
 
-from .model import MeasuredTuneResponseItem, MeasuredTuneResponsePerPowerConverter, MeasuredTuneResponse, TuneResponse, \
-    RandomVariableMomenta, TuneResponseCollection
+from .model import (
+    MeasuredTuneResponseItem,
+    MeasuredTuneResponsePerPowerConverter,
+    MeasuredTuneResponse,
+    TuneResponse,
+    RandomVariableMomenta,
+    TuneResponseCollection,
+)
 
 
 def data_to_model(data: Dict[str, Sequence[float]]) -> MeasuredTuneResponse:
@@ -18,21 +24,22 @@ def data_to_model(data: Dict[str, Sequence[float]]) -> MeasuredTuneResponse:
     # combine data per magnet
     tmp = defaultdict(list)
     for dev_name, val, tune_x, tune_y in zip_longest(
-            data["device_name"],
-            data["channel_value"],
-            data["tune-x-sig"],
-            data["tune-y-sig"]
+        data["device_name"],
+        data["channel_value"],
+        data["tune-x-sig"],
+        data["tune-y-sig"],
     ):
         tmp[str(dev_name)].append(
-            MeasuredTuneResponseItem(
-                value=float(val), x=float(tune_x), y=float(tune_y)
-            )
+            MeasuredTuneResponseItem(value=float(val), x=float(tune_x), y=float(tune_y))
         )
 
     # Now put it in a model
-    r = MeasuredTuneResponse(col=[
-            MeasuredTuneResponsePerPowerConverter(pc_name=dev_name, col=items) for dev_name, items in tmp.items()
-    ])
+    r = MeasuredTuneResponse(
+        col=[
+            MeasuredTuneResponsePerPowerConverter(pc_name=dev_name, col=items)
+            for dev_name, items in tmp.items()
+        ]
+    )
     return r
 
 
@@ -44,7 +51,7 @@ def fit_line(indep: Sequence[float], dep: Sequence[float]) -> RandomVariableMome
 
         orbit response profits from leastsquare `lstsg` so perhaps also to use it here
     """
-    coeffs, info =  Polynomial.fit(indep, dep, deg=1, full=True)
+    coeffs, info = Polynomial.fit(indep, dep, deg=1, full=True)
     residuals, rank, singular_values, rcond = info
     return RandomVariableMomenta(mean=float(coeffs.coef[1]), std=float(np.nan))
 
@@ -56,7 +63,7 @@ def fit_one_power_converter(data: MeasuredTuneResponsePerPowerConverter):
     return TuneResponse(pc_name=data.pc_name, x=xrm, y=yrm)
 
 
-def tune_response_analysis(data: Dict[str, Sequence[float]]):
+def tune_response_analysis(data: Dict[str, Sequence[float]]) -> TuneResponseCollection:
     prep_data = data_to_model(data)
     return TuneResponseCollection(
         col=[fit_one_power_converter(data) for data in prep_data.col]
