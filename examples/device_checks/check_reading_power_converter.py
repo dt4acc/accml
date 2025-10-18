1b6ecfcd85d48d8f9e467929298cb941e47f7e85
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2025-10-18 12:47:45 +0200

    i[TASK] code clean up

--- examples/device_checks/check_reading_power_converter.py
+++ examples/device_checks/check_reading_power_converter.py
@@ -1,20 +1,17 @@
 import asyncio
 import logging
-import time
 
+from typing import Annotated as A
 
 logging.basicConfig(level=logging.WARNING)
 
-from ophyd_async.core import StandardReadable, AsyncStatus, AsyncStageable
 from ophyd_async.tango.core import  tango_signal_r, tango_signal_rw
-from bluesky.protocols import Movable, Stoppable, SyncOrAsync, Stageable, Reading
 from bluesky import RunEngine
 import bluesky.plans as bpp
 from databroker import catalog
 from ophyd_async.tango.core import TangoReadable
 
-from typing import Annotated as A
-from ophyd_async.core import DEFAULT_TIMEOUT, AsyncStatus, SignalR, SignalRW, SignalX
+from ophyd_async.core import SignalR, SignalRW
 from ophyd_async.core import StandardReadableFormat as Format
 from ophyd_async.tango.core import TangoPolling
 
@@ -24,66 +21,10 @@ class Powerconverter(TangoReadable):
     current_readback: A[SignalR[float], TangoPolling(0.1, 0.1, 0.1)]
 
 
-class Powerconverter2(TangoReadable):
-    """a power converter that allows overriding setpoint and readback suffix"""
-
-    def __init__(
-        self,
-        prefix: str,
-        setpoint_suffix: str = None,
-        readback_suffix: str = None,
-        name="",
-        **kwargs,
-    ):
-        assert readback_suffix is not None
-        assert setpoint_suffix is not None
-
-        # with self.add_children_as_readables():
-            # Todo: add hints
-            # fmt:off
-        self.setpoint = tango_signal_rw(float , f"{prefix}/{setpoint_suffix}", name=f"{prefix}-setp")
-        self.readback = tango_signal_r(float, f"{prefix}/{readback_suffix}", name=f"{prefix}-rdbk")
-        self.add_readables([self.setpoint, self.readback])
-        super().__init__(name=name, **kwargs)
-
-    # async def stop(self, success=True) -> SyncOrAsync[None]:
-    #     self._set_success = success
-
-    # async def read(self) -> dict[str, Reading]:
-    #    d = (await self.readback.read()).copy()
-    #    d.update(await self.setpoint.read())
-    #    return d
-
-    # @AsyncStatus.wrap
-    # async def stage(self) -> None:
-    #      print("Staging power converter: get future ")
-    #
-    #     # await self.setpoint.stage()
-    #     # await self.readback.stage()
-    #
-    #     st = super().stage()
-    #     print("Staging power converter: await future ")
-    #     r =  await st
-    #     print("Staging power converter: return result")
-    #     return  r
-    #
-    # async def unstage(self) -> None:
-    #     print("Untaging power converter: get future ")
-    #     st = super().stage()
-    #     print("Unstaging power converter: await future ")
-    #     r =  await st
-    #     print("Unstaging power converter: return result")
-    #     return  r
-
-
-
-
 async def run_device():
     pc = Powerconverter(
         name="pc",
         trl="SimpleTangoServer/test/power_converter_Q3P2T6R",
-        # setpoint_suffix="current_setpoint",
-        # readback_suffix="current_readback"
     )
     await pc.connect()
     await pc.stage()
@@ -99,8 +40,6 @@ def main():
     pc = Powerconverter(
         name="pc",
         trl="SimpleTangoServer/test/power_converter_Q3P2T6R",
-        # setpoint_suffix="current_setpoint",
-        # readback_suffix="current_readback"
     )
 
     async def connect():
@@ -114,10 +53,9 @@ def main():
     uid, = RE(bpp.count([pc], 3))
     run = cat[uid]
     print(run.primary.read())
-     #print(uid)
-    # time.sleep(3)
 
 
 if __name__ == "__main__":
-    # asyncio.run(run_device())
-    main()
+    # you can only run one at once
+    asyncio.run(run_device())
+    # main()
