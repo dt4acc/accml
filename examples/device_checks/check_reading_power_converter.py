0bdc2ab21905a73d6c071016e56ab714c09a00bd
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2025-10-29 14:55:04 +0100

    [TASK] singled out boiler plate code, test executes +/-1  diff current

--- examples/device_checks/check_reading_power_converter.py
+++ examples/device_checks/check_reading_power_converter.py
@@ -1,64 +1,77 @@
 import asyncio
 import logging
-
 from typing import Annotated as A
-
-from bluesky.protocols import Reading
-from event_model import DataKey
 
 logging.basicConfig(level=logging.WARNING)
 
-from ophyd_async.tango.core import  tango_signal_r, tango_signal_rw
-from ophyd_async.core import derived_signal_rw, AsyncStatus, Device, AsyncStageable
+from ophyd_async.core import (
+    derived_signal_rw,
+    AsyncStageable,
+    AsyncStatus,
+    SignalR,
+    SignalRW,
+    StandardReadableFormat as Format,
+)
+from ophyd_async.tango.core import TangoPolling, TangoReadable
 
 from bluesky import RunEngine
 import bluesky.plans as bpp
 from databroker import catalog
-from ophyd_async.tango.core import TangoReadable, TangoDevice
-
-from ophyd_async.core import SignalR, SignalRW
-from ophyd_async.core import StandardReadableFormat as Format
-from ophyd_async.tango.core import TangoPolling
 
 
-class Powerconverter(TangoReadable, TangoDevice, AsyncStageable):
-    """
-    Todo:
-        need to find out why signals must be marked as uncached...
-        need to find out why only hinted signals end up in the output
-    """
-    current_setpoint: A[SignalRW[float], Format.HINTED_UNCACHED_SIGNAL, TangoPolling(1.0, 0.01, 1e-4)]
-    current_readback: A[SignalR[float], Format.UNCACHED_SIGNAL, TangoPolling(1.0, 0.01, 1e-4)]
-
-    def __init__(self, trl: str | None = "", name=""):
+class PowerconverterBase(TangoReadable, AsyncStageable):
+    def __init__(self, trl: str | None = "", name="", *, set_signal_name: str):
         super().__init__(trl, name=name)
         self._ref_current = None
+        # current tango version: signals are the names of the properties of the
+        # tango device. These have to be set accordingly
+
+        self._set_signal = getattr(self, set_signal_name)
         self.diff_current = derived_signal_rw(
-            self._to_relative_current, self._set_diff_current, current=self.current_setpoint, derived_precision=5,
+            self._to_relative_current,
+            self._set_diff_current,
+            current=self._set_signal,
+            derived_precision=5,
         )
-        self.add_readables([self.diff_current], Format.UNCACHED_SIGNAL)
+        self.add_readables([self.diff_current])
 
     def _to_relative_current(self, current: float) -> float:
-        assert self._ref_current is not None, "reference current None, was device staged?"
+        assert (
+            self._ref_current is not None
+        ), "reference current None, was device staged?"
         r = current - self._ref_current
-        self.log.debug(f"Computing relative current {current} - {self._ref_current}-> {r}")
+        self.log.debug(
+            f"Computing relative current {current} - {self._ref_current}-> {r}"
+        )
         return r
 
     async def _set_diff_current(self, dv: float):
-        current  = dv + self._ref_current
+        current = dv + self._ref_current
         self.log.debug(f"Setting {dv=} -> {current=}")
-        await self.current_setpoint.set(current)
-        return
-        rdbk = await self.current_setpoint._cache.backend.get_value()
-        setp = await self.current_setpoint._cache.backend.get_setpoint()
-        self.log.debug(f"Setting {dv=} -> {current=}: {setp=}, {rdbk=}")
+        await self._set_signal.set(current)
+        if self.log.getEffectiveLevel() >= logging.DEBUG:
+            rdbk = await self._set_signal._cache.backend.get_value()
+            setp = await self._set_signal._cache.backend.get_setpoint()
+            self.log.debug(f"Setting {dv=} -> {current=}: {setp=}, {rdbk=}")
+
+    async def read(self):
+        """
+        Todo:
+            need to find out why I have to call read explicitly
+        """
+        d = await super().read()
+        entry = d[self._set_signal.name]
+        d[self.diff_current.name] = dict(
+            value=self._to_relative_current(entry["value"]),
+            timestamp=entry["timestamp"],
+        )
+        return d
 
     @AsyncStatus.wrap
     async def stage(self) -> None:
         self.log.info("staging")
         r = await super().stage()
-        # todo: set it with actual current
-        self._ref_current = await self.current_setpoint.get_value()
+        self._ref_current = await self._set_signal.get_value()
         return r
 
     @AsyncStatus.wrap
@@ -68,30 +81,18 @@ class Powerconverter(TangoReadable, TangoDevice, AsyncStageable):
         self._ref_current = None
         return r
 
-    async def describe(self) -> dict[str, DataKey]:
-        """
 
-        Todo:
-            need to find out why I have to add that here
-            is an uncached signal arriving here
-        """
-        d = await super().describe()
-        return d
+class Powerconverter(PowerconverterBase):
+    """
+    Todo:
+        need to find out why signals must be marked as uncached...
+    """
 
-    async def read(self) -> dict[str, Reading]:
-        """
+    # fmt: off
+    current_setpoint: A[ SignalRW[float] , Format.HINTED_UNCACHED_SIGNAL, TangoPolling(1.0, 0.01, 1e-4) ]
+    current_readback: A[ SignalR[float] ,  Format.UNCACHED_SIGNAL,        TangoPolling(1.0, 0.01, 1e-4) ]
+    # fmt: on
 
-        Todo:
-            need to find out how I need to configure the derived signal so that it will
-            be always read out
-        """
-        d = await super().read()
-        return d
-        src_name = self.current_setpoint.name
-        dst_name = self.diff_current.name
-        r = d.get(src_name)
-        d[dst_name] = Reading(value=self._to_relative_current(r.get("value")), timestamp=r.get("timestamp"))
-        return d
 
 async def run_device():
     pc = Powerconverter(
@@ -107,13 +108,13 @@ async def run_device():
     await pc.unstage()
 
 
-
 def main():
-
+    # need some catalog so that I can read back the data
     cat = catalog["heavy_local"]
     pc = Powerconverter(
         name="pc",
         trl="SimpleTangoServer/test/power_converter_Q3P2T6R",
+        set_signal_name="current_setpoint",
     )
 
     async def connect():
@@ -124,10 +125,10 @@ def main():
 
     RE = RunEngine()
     RE.subscribe(cat.v1.insert)
-    # uid, = RE(bpp.count([pc], 3))
-    # run = cat[uid]
-    # print(run.primary.read())
-    uid, = RE(bpp.scan([pc], pc.diff_current, 0, 1, 3))
+    (uid,) = RE(bpp.count([pc], 3))
+    run = cat[uid]
+    print(run.primary.read())
+    (uid,) = RE(bpp.list_scan([pc], pc.diff_current, [0, 1, -1, 0]))
     run = cat[uid]
     print(run.primary.read())
 
