e960455f3df8b24cbf5208f01d01d9d097e0bb93
Waheed <wsulimankhail@gmail.com>
2025-12-11 10:55:52 +0100

    Extract accml_lib into standalone repository

--- src/accml/custom/accml_lib/bessyii/set_command_rewriter.py
+++
@@ -1,7 +0,0 @@
-from .liasion_translator_setup import load_managers
-from ..bl.command_rewritter import CommandRewriter
-
-
-def set_command_rewriter():
-    _, lm, tm = load_managers()
-    return CommandRewriter(liaison_manager=lm, translation_service=tm)
