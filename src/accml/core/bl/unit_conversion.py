59525a999b3835578b9481ad1769966fd6a73777
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2025-12-23 12:01:13 +0100

    [TASK] linear (energy independent) conversion added

--- src/accml/core/bl/unit_conversion.py
+++ src/accml/core/bl/unit_conversion.py
@@ -6,8 +6,7 @@ logger = logging.getLogger("accml")
 
 
 class EnergyDependentLinearUnitConversion(StateConversion):
-    """Typical example: magnet parameters
-    """
+    """Typical example: magnet parameters"""
 
     def __init__(self, *, intercept: float, slope: float, brho: float):
         self.intercept = intercept
@@ -15,15 +14,56 @@ class EnergyDependentLinearUnitConversion(StateConversion):
         self.brho = brho
 
     def forward(self, state: float) -> float:
-        logger.info("%s.forward: brho %s, intercept %s slope %s, state %s", self.__class__.__name__, self.brho,
-                    self.intercept, self.slope, state)
+        logger.info(
+            "%s.forward: brho %s, intercept %s slope %s, state %s",
+            self.__class__.__name__,
+            self.brho,
+            self.intercept,
+            self.slope,
+            state,
+        )
         intercept = self.intercept * self.brho
         slope = self.slope * self.brho
         return intercept + slope * state
 
     def inverse(self, state: float) -> float:
-        logger.info("%s.inverse: brho %s, intercept %s slope %s, state %s", self.__class__.__name__, self.brho,
-                    self.intercept, self.slope, state)
+        logger.info(
+            "%s.inverse: brho %s, intercept %s slope %s, state %s",
+            self.__class__.__name__,
+            self.brho,
+            self.intercept,
+            self.slope,
+            state,
+        )
         intercept = self.intercept * self.brho
         slope = self.slope * self.brho
         return (state - intercept) / slope
+
+
+class LinearUnitConversion(StateConversion):
+    """Typical example: tune"""
+
+    def __init__(self, *, intercept: float, slope: float):
+        self.intercept = intercept
+        self.slope = slope
+
+    def forward(self, state: float) -> float:
+        logger.info(
+            "%s.forward: intercept %s slope %s, state %s",
+            self.__class__.__name__,
+            self.intercept,
+            self.slope,
+            state,
+        )
+        return self.intercept + self.slope * state
+
+    def inverse(self, state: float) -> float:
+        logger.info(
+            "%s.inverse: brho %s, intercept %s slope %s, state %s",
+            self.__class__.__name__,
+            self.brho,
+            self.intercept,
+            self.slope,
+            state,
+        )
+        return (state - self.intercept) / self.slope
