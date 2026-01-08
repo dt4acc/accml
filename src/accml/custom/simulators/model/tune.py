dd3488cfa23e45303d0125e12dd38d225d95a2c5
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2026-01-08 10:45:07 +0100

    [TASK] tune model: should it be moved to core/model?

--- src/accml/custom/simulators/model/tune.py
+++ src/accml/custom/simulators/model/tune.py
@@ -2,7 +2,7 @@
 
 Todo:
     merge it with app?
-    move it to core
+    move it to core?
 """
 from dataclasses import dataclass
 
@@ -11,9 +11,20 @@ from dataclasses import dataclass
 class Tune:
     """
     Todo:
-        more it with accml.app.tune.model.MeasuredTuneResponseItem?
+        merge
+         it with accml.app.tune.model.MeasuredTuneResponseItem?
     """
+
     #: horizontal
     x: float
     #: vertical
     y: float
+
+    def __sub__(self, other):
+        return Tune(self.x - other.x, self.y - other.y)
+
+    def __neg__(self):
+        return Tune(-self.x, -self.y)
+
+    def __str__(self):
+        return f"{self.__class__.__name__}(x={self.x:.4f}, y={self.y:.4f})"
