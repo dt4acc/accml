7f9d23bbbf7a4777485bc4dc4bd7dfe103eb13db
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2026-01-12 11:09:26 +0100

    [TASK] storage with interface

---
+++ src/accml/core/utils/simple_storage.py
@@ -0,0 +1,21 @@
+from accml.core.interfaces.storage import StorageInterface
+from uuid import uuid4
+
+
+class SimpleDataStorage(StorageInterface):
+    def __init__(self):
+        self.data = dict()
+
+    def add(self, data) -> str:
+        uuid = uuid4()
+        self.data[uuid] = data
+        return uuid
+
+    def clear(self):
+        self.data.clear()
+
+    def keys(self):
+        return self.data.keys()
+
+    def get(self, uuid: str):
+        return self.data.get(uuid)
