fbcb5981c3aed1e1fe30adbe896d5a36d9d96ecb
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2026-01-23 14:21:51 +0100

    [TASK] added ophyd async based simulation code

---
+++ examples/tune/ophyd_async_based/tune_response_measurement.py
@@ -0,0 +1,76 @@
+import json
+
+
+import jsons
+
+from accml.app.tune.tune_measurement import measure_tune_response
+from accml.core.utils.basic_measurement_execution_engine import BasicMeasurementExecutionEngine
+from accml.core.utils.simple_storage import SimpleDataStorage
+from accml.custom.ophyd_async.ophyd_async_backend import OphydAsyncDeviceBackendRW
+from accml.custom.ophyd_async.ophyd_async_measurement_execution_engine import OphydAsyncDeltaBackendRWProxy
+from accml_lib.core.bl.command_rewritter import CommandRewriter
+from accml_lib.core.bl.delta_backend import StateCache
+from accml_lib.core.model.utils.command import ReadCommand
+from accml_lib.core.model.utils.identifiers import LatticeElementPropertyID
+from accml_lib.core.model.output.result import register_serializers_to_json_fork
+from accml_lib.custom.bessyii.liasion_translator_setup import load_managers
+from accml_lib.custom.bessyii.setup import setup
+
+jsons_fork = jsons.fork()
+register_serializers_to_json_fork(jsons_fork)
+
+
+async def main():
+    yp, lm, ts = load_managers()
+    devices = setup(prefix=None)
+
+    # Todo: remove this line after only Q3/Q4 are flagged as tune correction magnets
+    tune_correction_quads = [name for name in yp.tune_correction_quadrupole_names() if name[1] in ["3", "4"]]
+    #  Find out to which power converters these are connected to
+    pc_names = {
+        lm.forward(LatticeElementPropertyID(name, "main_strength")).device_name:
+            name for name in tune_correction_quads
+    }
+    # Now I add a hack: I only use quadrupoles whoes power converter is unique
+    # I should rather work in device space right away
+    pc_names=list(set(pc_names))
+    # here for demo only do it for two magnets
+    pc_names = pc_names[:2]
+    storage = SimpleDataStorage()
+
+    await devices.get("tune").connect()
+    await devices.get('quadrupole_pcs').connect()
+
+    backend = OphydAsyncDeltaBackendRWProxy(
+        OphydAsyncDeviceBackendRW(devices=devices),
+        cache=StateCache(name="BESSY2_OphydAsync_Dev_State_Cache"),
+    )
+    mexec = BasicMeasurementExecutionEngine(
+        backend=backend,
+        cmd_rewriter=CommandRewriter(liaison_manager=lm, translation_service=ts),
+        storage=storage,
+        expected_view_for_output="device",
+        num_readings=3
+    )
+
+    uuid = await measure_tune_response(
+       detectors=[
+           ReadCommand("tune", "transversal"),
+       ],
+       quadrupole_pc_names=pc_names,
+       measurement_values=[0, 1e0, 0, -1e0, 0],
+       mexec=mexec,
+       info_signals=None,
+        #
+        n_sample=2
+    )
+    data = storage.get(uuid)
+    data = jsons.dump(data, fork_inst=jsons_fork)
+
+    with open("tune_response_data_with_ophyd_async.json", "wt") as fp:
+        json.dump(data, fp, indent=4)
+
+
+if __name__  == "__main__":
+    import asyncio
+    asyncio.run(main())
