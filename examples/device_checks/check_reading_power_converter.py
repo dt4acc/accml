266773d2a10c1fc870bfe40971991920a3dfd6f8
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2026-01-29 16:17:07 +0100

    [TASK] adapted device checks to HZB's digital twin

--- examples/device_checks/check_reading_power_converter.py
+++ examples/device_checks/check_reading_power_converter.py
@@ -8,6 +8,7 @@ from accml.custom.tango.devices.power_converter import PowerConverter
 from bluesky import RunEngine
 import bluesky.plans as bpp
 from databroker import catalog
+import bluesky.plan_stubs as bps
 
 import tango
 
@@ -15,11 +16,13 @@ print(tango.__version__)
 
 
 async def run_device():
+    trl = "SimpleTangoServer/test/power_converter_Q3P2T6R"
+    trl = "AN01-AR/EM/CQLN.03-pc"
     pc = PowerConverter(
         name="pc",
-        trl="SimpleTangoServer/test/power_converter_Q3P2T6R",
-        setpoint_name="current_setpoint",
-        readback_name="current_readback",
+        trl=trl,
+        # setpoint_name="current_setpoint",
+        # readback_name="current_readback",
     )
     t = await pc.connect()
     t
@@ -32,12 +35,14 @@ async def run_device():
 
 def main():
     # need some catalog so that I can read back the data
+    trl = "SimpleTangoServer/test/power_converter_Q3P2T6R"
+    trl = "AN01-AR/EM/CQLN.03-pc"
     cat = catalog["heavy_local"]
     pc = PowerConverter(
         name="pc",
-        trl="SimpleTangoServer/test/power_converter_Q3P2T6R",
-        setpoint_name="current_setpoint",
-        readback_name="current_readback",
+        trl=trl,
+        # setpoint_name="current_setpoint",
+        # readback_name="current_readback",
     )
 
     async def connect():
@@ -51,13 +56,19 @@ def main():
     (uid,) = RE(bpp.count([pc], 3))
     run = cat[uid]
     print(run.primary.read())
-    (uid,) = RE(bpp.list_scan([pc], pc.delta_set_current, [0, 1, -1, 0]))
+
+    def per_step(detectors, motor, step):
+        yield from bps.mv(motor, step)
+        yield from bps.sleep(2.0)
+        yield from bps.trigger_and_read(detectors + [motor])
+
+    (uid,) = RE(bpp.list_scan([pc], pc.current_set, [0, 1, -1, 0], per_step=per_step))
     run = cat[uid]
     print(run.primary.read())
 
 
 if __name__ == "__main__":
     # you can only run one at once
-    # asyncio.run(run_device())
-    main()
+    asyncio.run(run_device())
+    # main()
     pass
--- examples/device_checks/check_reading_tune.py
+++ examples/device_checks/check_reading_tune.py
@@ -3,13 +3,20 @@ import pprint
 
 from accml.custom.tango.devices.tunes import Tunes
 
+
 async def run_device():
-    tune = Tunes(name="tunes", trl="SimpleTangoServer/test/tune_device")
+    trl = "SimpleTangoServer/test/tune_device"
+    trl = "PHYSICS/SOLEIL/TUNE"
+    tune = Tunes(name="tunes", trl=trl)
     await tune.connect()
     await tune.stage()
     d = await tune.describe()
+    r1 = await tune.read()
     r = await tune.read()
     pprint.pprint(r)
+    dT_x = r["tunes-hor"]["timestamp"] - r1["tunes-hor"]["timestamp"]
+    dT_y = r["tunes-vert"]["timestamp"] - r1["tunes-vert"]["timestamp"]
+    pprint.pprint(f"tune change r1 -> r x {dT_x}, {dT_y}")
     await tune.unstage()
 
 def main():
